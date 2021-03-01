cc0c35d213f2d8f2e013526dc0c5d44bb09a8d8d
José Leonel López Ajvix <jlopez-2019044@kinal.edu.gt>
2021-02-28 21:24:33 -0600

    Se creo el jwt y authenticated de usuario

--- src/controladores/usuario.controlador.js
+++ src/controladores/usuario.controlador.js
@@ -2,6 +2,8 @@
 
 const Usuario = require('../modelos/usuarios.model');
 const bcrypt = require('bcrypt-nodejs');
+const jwt = require('../servicios/jwt');
+const usuarioModelos = require('../modelos/usuarios.model')
 
 
 //FUNCION PARA CREAR EL USUARIO PREDETERMINADO
@@ -16,6 +18,7 @@ function usuarioDefault(req,res){
         {usuario: usuarioModel.usuario},
         {rol: usuarioModel.rol}
     ]}).exec((err, usuariosEncontrados)=>{
+            
         if(err) return console.log('Error en la petición del usuario')
         if(usuariosEncontrados && usuariosEncontrados.length>=1){
             console.log("Usuario ADMIN ya está creado")
@@ -23,17 +26,50 @@ function usuarioDefault(req,res){
             bcrypt.hash(usuarioModel.password,null,null,(err,passwordEncriptada)=>{
                 usuarioModel.password = passwordEncriptada;
             })
-
+    
             usuarioModel.save((err, usuarioGuardado)=>{
                 if(err) return console.log("Error al guardar el usuario");
-
+    
                 if(usuarioGuardado){
                     console.log('usuario Guardado '+usuarioGuardado);
                 }
             })
         }
+
     })
 
+}
+
+function login(req,res) {
+
+    var params = req.body;
+
+    Usuario.findOne({usuario: params.usuario},(err,usuarioEncontrado)=>{
+        if(err) return res.status(500).send({mensaje: 'Error en la petición'})
+
+        if(usuarioEncontrado){
+            bcrypt.compare(params.password,usuarioEncontrado.password,(err,passCorrecta)=>{
+
+                if(passCorrecta){
+                    if(params.obtenerToken ==='true'){
+                        return res.status(200).send({
+                            token: jwt.createToken(usuarioEncontrado)
+                        });
+                    }else{
+                        usuarioEncontrado.password = undefined;
+                        return res.status(200).send({usuarioEncontrado})
+                    }
+                }else{
+                    return res.status(404).send({mensaje: 'El usuario no se ha podido identificar'})
+                }
+
+            })
+        }else{
+            return res.status(404).send({mensaje: 'El usuario no ha podido ingresar'})
+        }
+
+    })
+    
 }
 
 function agregarUsuario(req,res) {
@@ -49,7 +85,10 @@ function agregarUsuario(req,res) {
         Usuario.find({$or: [
             {usuario: usuarioModel.usuario}
         ]}).exec((err, usuariosEncontrados)=>{
-            if(err) return res.status(500).send({mensaje: 'Error en la petición del usuario'})
+
+            if(req.user.rol === 'ROL_ADMIN'){
+
+                if(err) return res.status(500).send({mensaje: 'Error en la petición del usuario'})
 
             if(usuariosEncontrados && usuariosEncontrados.length>=1){
                 return res.status(500).send({mensaje: 'El usuario ya existe'})
@@ -69,6 +108,11 @@ function agregarUsuario(req,res) {
 
                 })
             }
+
+            }else{
+                return res.status(500).send({mensaje: 'No es un Administrador así que no puede crear uno'})
+            }
+
         })
     }
     
@@ -76,5 +120,6 @@ function agregarUsuario(req,res) {
 
 module.exports ={
     usuarioDefault,
-    agregarUsuario
+    agregarUsuario,
+    login
 }
